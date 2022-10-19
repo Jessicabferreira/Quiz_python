071e09bccd5154c8f42e265c247dedc8509fd792
Jessica Ferreira <jessiberteloni@gmail.com>
2022-10-19 16:55:43 -0300

    Perguntas e respostas

--- quiz.py
+++ quiz.py
@@ -1,34 +1,162 @@
-print('Seja bem vindo ao quiz da Jessica!')
-print('Ai vai algumas perguntas sobre a linguagem Python!')
+print(''' 
+    Seja bem vindo ao quiz da Jessica!
+    
+    Ai vai algumas perguntas sobre Alimentação Saudável!
 
-usuario = input('Quer começar? (S/N) ').upper()
-print(usuario)
+''')
+print('Vamos começar...\n')
 
-if usuario != 'S':
-    quit()
+print('1- Entende-se por gordura trans: \n ')
+print('(A) É o tipo de gordura encontrada nos alimentos de origem animal, como manteiga e queijo \n')
+print('(B) É encontrada em alimentos de origem vegetal, como óleos de milho, soja e canola \n')
+print('(C) É um tipo de gordura formada por um processo de hidrogenação natural ou industrial, portanto, está presente '
+'em alimentos industrializados \n')
+print('(D) É considerada a gordura que faz bem à saúde \n')
 
+resposta_1 = input('Digite uma das opções: \n').upper()
 pontos = 0
 
-print('Começando... \n')
-print('O que é Python? \n (A) Aplicativo \n (B) Documento \n (C) Linguagem de programação \n (D) IDE \n')
-resposta_1 = input('Resposta: \n').upper()
-
 if resposta_1 == 'C':
-    print('Correto')
+    print('Correto\n')
+    pontos += 20   # pontos = pontos + 20
+else:
+    print('Incorreto\n')
+
+print('2- А vіtаmіnа А tеm соmо funçõеѕ fоrtаlесеr а vіѕãо, mаntеr а реlе, оѕ саbеlоѕ е аѕ unhаѕ ѕаudávеіѕ, соntrіbuіr para'
+'а рrеvеnçãо dе dоеnçаѕ е рrоmоvеr о сrеѕсіmеntо óѕѕео. Еlа é еnсоntrаdа еm mаіоr quаntіdаdе еm quаіѕ аlіmеntоѕ? \n')
+print('(A) Fígado, peixes, cenoura, tomate, espinafre \n')
+print('(B) Vegetais folhosos, abacaxi, morango, goiaba \n')
+print('(C) Grãos, cereais integrais, ervilha \n')
+print('(D) Gérmen de trigo, caju, cenoura \n')
+
+resposta_2 = input('Digite uma das opções: \n').upper()
+pontos = 0
+
+if resposta_2 == 'A':
+    print('Correto\n')
     pontos = pontos + 1
 else:
     print('Incorreto\n')
-print()
-print('O simbolo # é usuado para? \n (A) Pular linha \n (B) Deixar a palavra em negrito \n (C) Separae as palavras \n '
-'(D) Fazer comentario \n')
-resposta_2 = input('resposta: \n').upper()
 
-if resposta_2 == 'D':
-    print('Correto')
+print('3- О соrро рrесіѕа dе quаntіdаdеѕ реquеnаѕ dе gоrdurа, рrіnсіраlmеntе раrа рrоduzіr mеmbrаnаѕ сеlulаrеѕ; quаlquеr'
+'ехсеѕѕо é dеѕnесеѕѕárіо, dеѕѕа mаnеіrа nãо há nесеѕѕіdаdе dе ѕе соnѕumіr gоrdurа numа dіеtа. Еѕtа frаѕе é vеrdаdеіrа'
+'оu fаlѕа? \n')
+print('(A) V \n')
+print('(B) F \n')
+
+resposta_3 = input('Digite uma das opções: \n').upper()
+pontos = 0
+
+if resposta_3 == 'A':
+    print('Correto\n')
     pontos = pontos + 1
 else:
-    print('Incorreto')
+    print('Incorreto \n')
 
-print(f'Quiz acabou... Pontuação: {pontos}/2')
+print('4- Quais os nomes comuns de lactose e frutose?')
+print('(A) Leite e Frutas \n')
+print('(B) Laticínios e Frutas \n')
+print('(C) Açúcar do Leite e Açúcar das Frutas \n')
+
+resposta_4 = input('Digite uma das opções: \n').upper()
+pontos = 0
+
+if resposta_4 == 'C':
+    print('Correto\n')
+    pontos = pontos + 1
+else:
+    print('Incorreto \n')
+
+print('5- Que tipo conhecido de substância química alimentícia é um carboidrato completo? \n')
+print('(A) Glicose \n')
+print('(B) Amido \n')
+print('(C) Glicídio \n')
+
+resposta_5 = input('Digite uma das opções: \n').upper()
+pontos = 0
+
+if resposta_5 == 'B':
+    print('Correto\n')
+    pontos = pontos + 1
+else:
+    print('Incorreto \n')
+
+print('6- О аlіmеntо zеrо é аquеlе quе nãо соntém аlgum nutrіеntе еѕресífісо. Роdе ѕеr zеrо еm glútеn, еm lасtоѕе, еm'
+'аçúсаr, еtс. Еѕtеѕ аlіmеntоѕ ѕãо bеnéfісоѕ раrа quеm рrесіѕа ехсluіr um nutrіеntе еѕресífісо dа ѕuа dіеtа, ѕеја роr'
+'рrоblеmаѕ mеtаbólісоѕ, аlеrgіаѕ оu іntоlеrânсіаѕ.Еѕѕа аfіrmаçãо é vеrdаdеіrа оu fаlѕа? \n')
+print('(A) V \n')
+print('(B) F \n')
+
+resposta_6 = input('Digite uma das opções: \n').upper()
+pontos = 0
+
+if resposta_6 == 'A':
+    print('Correto\n')
+    pontos = pontos + 1
+else:
+    print('Incorreto \n')
+
+print('7- Um аlіmеntо é оrgânісо quаndо ѕеuѕ grãоѕ nãо раѕѕаrаm роr nеnhum рrосеѕѕо dе rеfіnаçãо. Роr іѕѕо, еlеѕ соnѕеrvаm'
+'tоdоѕ ѕеuѕ соmроnеntеѕ оrіgіnаіѕ, іnсluіndо саѕсаѕ е реlíсulаѕ рrоtеtоrаѕ. Еѕѕа аfіrmаçãо é vеrdаdеіrа оu fаlѕа? \n')
+print('(A) V \n')
+print('(B) F \n')
+
+resposta_7 = input('Digite uma das opções: \n').upper()
+pontos = 0
+
+if resposta_7 == 'B':
+    print('Correto\n')
+    pontos = pontos + 1
+else:
+    print('Incorreto \n')
+
+print('8- Qual dos alimentos a seguir é o mais saudável? \n')
+print('(A) Verduras \n')
+print('(B) Frutas \n')
+print('(C) Óleos vegetais \n')
+print('(D) Leite e derivados \n')
+print('(E) Legumes \n')
+
+resposta_8 = input('Digite uma das opções: \n').upper()
+pontos = 0
+
+if resposta_8 == 'C':
+    print('Correto\n')
+    pontos = pontos + 1
+else:
+    print('Incorreto \n')
+
+print('9- Complete a frase: ___ é utilizado na fabricação de alguns hormônios. \n')
+print('(A) Lipídio \n')
+print('(B) Proteína, porque tem função construtora \n')
+print('(C) Vitamina \n')
+print('(D) Nenhuma das alternativas está correta \n')
+
+resposta_9 = input('Digite uma das opções: \n').upper()
+pontos = 0
+
+if resposta_9 == 'A':
+    print('Correto\n')
+    pontos = pontos + 1
+else:
+    print('Incorreto \n')
+
+print('10- Qual nutriente tem função construtora com principal função? \n')
+print('(A) Carboidratos \n')
+print('(B) Sais Minerais \n')
+print('(C) Fibras \n')
+print('(D) Proteínas \n')
+print('(E) Vitaminas \n')
+
+resposta_10 = input('Digite uma das opções: \n').upper()
+pontos = 0
+
+if resposta_10 == 'A':
+    print('Correto\n')
+    pontos = pontos + 1
+else:
+    print('Incorreto \n')
+
+print(f'Quiz acabou... Pontuação: {pontos}/10')
 
 
