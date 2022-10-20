e9da42720fdafe3fe6ab5c3cc419182ce335df14
Jessica Ferreira <jessiberteloni@gmail.com>
2022-10-20 14:54:54 -0300

    Perguntas e respostas

--- quiz.py
+++ quiz.py
@@ -13,8 +13,9 @@ print('(C) É um tipo de gordura formada por um processo de hidrogenação natur
 'em alimentos industrializados \n')
 print('(D) É considerada a gordura que faz bem à saúde \n')
 
-resposta_1 = input('Digite uma das opções: \n').upper()
 pontos = 0
+
+resposta_1 = input('Digite uma das opções: \n').upper()
 
 if resposta_1 == 'C':
     print('Correto! \n')
@@ -30,7 +31,6 @@ print('(C) Grãos, cereais integrais, ervilha \n')
 print('(D) Gérmen de trigo, caju, cenoura \n')
 
 resposta_2 = input('Digite uma das opções: \n').upper()
-pontos = 0
 
 if resposta_2 == 'A':
     print('Correto! \n')
@@ -45,7 +45,7 @@ print('(A) V \n')
 print('(B) F \n')
 
 resposta_3 = input('Digite uma das opções: \n').upper()
-pontos = 0
+
 
 if resposta_3 == 'A':
     print('Correto! \n')
@@ -59,7 +59,6 @@ print('(B) Laticínios e Frutas \n')
 print('(C) Açúcar do Leite e Açúcar das Frutas \n')
 
 resposta_4 = input('Digite uma das opções: \n').upper()
-pontos = 0
 
 if resposta_4 == 'C':
     print('Correto! \n')
@@ -73,7 +72,6 @@ print('(B) Amido \n')
 print('(C) Glicídio \n')
 
 resposta_5 = input('Digite uma das opções: \n').upper()
-pontos = 0
 
 if resposta_5 == 'B':
     print('Correto! \n')
@@ -88,7 +86,6 @@ print('(A) V \n')
 print('(B) F \n')
 
 resposta_6 = input('Digite uma das opções: \n').upper()
-pontos = 0
 
 if resposta_6 == 'A':
     print('Correto! \n')
@@ -102,7 +99,6 @@ print('(A) V \n')
 print('(B) F \n')
 
 resposta_7 = input('Digite uma das opções: \n').upper()
-pontos = 0
 
 if resposta_7 == 'B':
     print('Correto! \n')
@@ -118,7 +114,6 @@ print('(D) Leite e derivados \n')
 print('(E) Legumes \n')
 
 resposta_8 = input('Digite uma das opções: \n').upper()
-pontos = 0
 
 if resposta_8 == 'C':
     print('Correto! \n')
@@ -133,7 +128,6 @@ print('(C) Vitamina \n')
 print('(D) Nenhuma das alternativas está correta \n')
 
 resposta_9 = input('Digite uma das opções: \n').upper()
-pontos = 0
 
 if resposta_9 == 'A':
     print('Correto! \n')
@@ -149,7 +143,6 @@ print('(D) Proteínas \n')
 print('(E) Vitaminas \n')
 
 resposta_10 = input('Digite uma das opções: \n').upper()
-pontos = 0
 
 if resposta_10 == 'E':
     print('Correto! \n')
@@ -157,6 +150,6 @@ if resposta_10 == 'E':
 else:
     print('Incorreto! \n')
 
-print(f'Quiz acabou... Pontuação: {pontos}/10')
+print(f'Quiz acabou... Parabéns sua pontuação foi: {pontos}/10')
 
 
